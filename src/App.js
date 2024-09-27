0a589cb7d27f892a0507c9222925f42bebcd0611
Abdenour Garti <garti.abdenour@gmail.com>
2024-09-27 01:17:21 +0100

    steps next back

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { StepperContext } from "./contexts/StepperContex";
 import Stepper from "./components/Stepper";
 import StepperControl from "./components/StepperControl";
 import Account from "./components/steps/Account"
@@ -8,6 +9,8 @@ import Final from "./components/steps/Final"
 
 function App() {
   const [currentStep, setCurrentStep] = useState(1);
+  const [userData, setUserData] = useState('');
+  const [finalData, setFinalData] = useState([]);
   const steps = [
     "Account information",
     "Personal Details",
@@ -28,12 +31,28 @@ function App() {
       default:
     }
   }
+
+  const handleClick = (direction) => {
+    let newStep = currentStep;
+    direction === "next" ? newStep++ : newStep--;
+    newStep > 0 && newStep <= steps.length && setCurrentStep(newStep)
+  }
   return (
     <div className="md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white">
       <div className="container horizontal mt-5">
         <Stepper steps={steps} currentStep={currentStep}/>
+        <div className="my-10 py-10">
+          <StepperContext.Provider value={{
+            userData,
+            setUserData,
+            finalData,
+            setFinalData
+          }}>
+            {displayStep(currentStep)}
+          </StepperContext.Provider>
+        </div>
       </div>
-      <StepperControl />
+      <StepperControl handleClick={handleClick} steps={steps} currentStep={currentStep}/>
     </div>
   );
 }
